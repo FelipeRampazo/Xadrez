efa0818713520668de7c1ebe7286165e7962811f
Felipe Rampazo <felipe_a_r@hotmail.com>
2021-05-06 09:46:21 -0300

    adicionado a classe posicaoXadrez

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -8,14 +8,10 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8,8);
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(3, 4));
-
-            Tela.imprimirTabuleiro(tab);
-            Console.ReadLine();
-
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+           
+            
+            Console.WriteLine(pos.ToPosicao());
 
 
         }
---
+++ Xadrez-console/Tabuleiro/TabuleiroExcepition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class TabuleiroExcepition : Exception
+    {
+        public TabuleiroExcepition(string msg) : base(msg)
+        {
+
+        }
+
+    }
+}
